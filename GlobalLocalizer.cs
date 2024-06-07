d8fb5b5d46f2f35679bef04afea68c39465f5394
tonwin618 <tonwin618@qq.com>
2024-06-07 14:10:24 +0800

    feat: support multiple languages.

---
+++ GlobalLocalizer.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// 全局多语言资源对应索引类
+/// </summary>
+public class GlobalLocalizer
+{
+
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddControllers();
+var services = builder.Services;
+services.AddRazorPages().AddViewLocalization(options => options.ResourcesPath = "Resources");
+services.AddControllers();
 
+var supportedCultures = new []
+{
+    new CultureInfo("en"),
+    new CultureInfo("zh")
+};
+
+services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("zh");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
+services.AddLocalization(
+    options => options.ResourcesPath = "Resources");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +32,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//Localization
+app.UseRequestLocalization();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
