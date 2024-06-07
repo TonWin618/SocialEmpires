a464572a3e398e75f2bca1be1274276942c5105c
tonwin618 <tonwin618@qq.com>
2024-06-07 18:20:44 +0800

    feat: Add login

---
+++ Controllers/IdentityController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SocialEmpires.Models;
+using SocialEmpires.Pages;
+using SocialEmpires.Pages.Shared;
+using System.Security.Cryptography;
+
+namespace SocialEmpires.Controllers
+{
+    [ApiController]
+    public class IdentityController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityController(
+            AppDbContext appDbContext,
+            UserManager<IdentityUser> userManager) 
+        {
+            _appDbContext = appDbContext;
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task RegisterByEmailAndPassword(
+            [FromForm] RegisterByEmailAndPasswordRequest request)
+        {
+            var user = new IdentityUser()
+            {
+                Email = request.email,
+            };
+            await _userManager.CreateAsync(user, request.password);
+        }
+
+        public record RegisterByEmailAndPasswordRequest(string email, string password);
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginByEmailAndPassword(
+            [FromForm] LoginByEmailAndPasswordRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.email);
+            if(user == null)
+            {
+                TempData["ErrorMessage"] = "UserNotFound";
+                return Redirect("/Login");
+            }
+
+            if(await _userManager.CheckPasswordAsync(user, request.password))
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                ViewData["ErrorMessage"] = "InvalidPassword";
+                return Redirect("/Login");
+            }
+        }
+        public record LoginByEmailAndPasswordRequest(string email, string password);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using SocialEmpires.Models;
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,16 +23,57 @@ services.Configure<RequestLocalizationOptions>(options =>
 {
     options.DefaultRequestCulture = new RequestCulture("zh");
     options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
 });
 
 services.AddLocalization(
     options => options.ResourcesPath = "Resources");
 
+services.AddDbContext<AppDbContext>(options =>
+{
+    var dbstr = "Data Source = Models/data.db";
+    options.UseSqlite(dbstr, sqliteOptionsAction =>
+    {
+        sqliteOptionsAction.MigrationsAssembly(typeof(Program).Assembly.GetName().Name);
+    });
+});
+
+services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    options.User.RequireUniqueEmail = false;
+})
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+services.ConfigureApplicationCookie(cookie =>
+{
+    cookie.LoginPath = "/Login";
+    cookie.LogoutPath = "/Logout";
+    cookie.AccessDeniedPath = "/AccessDenied";
+    cookie.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    // cookie.SlidingExpiration = true;
+});
+
 var app = builder.Build();
 
 app.UseRequestLocalization();
 app.UseAuthorization();
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
