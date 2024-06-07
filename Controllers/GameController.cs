b316351e92705cb3c564d858cb4547e65c6fb641
tonwin618 <tonwin618@qq.com>
2024-06-07 15:26:06 +0800

    feat: GetCrossdomainXmlFile

---
+++ Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialEmpires.Controllers
+{
+    public class GameController : ControllerBase
+    {
+        [HttpGet("crossdomain.xml")]
+        public ActionResult GetCrossdomainXmlFile()
+        {
+            return SendFromLocal("crossdomain.xml", "application/xml");
+        }
+
+        private PhysicalFileResult SendFromLocal(string relativePath, string contentType)
+        {
+            return PhysicalFile(
+                Directory.GetCurrentDirectory() + "/Assets/" + relativePath, 
+                contentType);
+        }
+    }
+}
