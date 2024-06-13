9606a1766e20314e8d96a1cc30cb25646f976095
tonwin618 <tonwin618@qq.com>
2024-06-13 16:37:31 +0800

    feat: implement PlayerSaveService

--- Services/PlayerSaveService.cs
+++ Services/PlayerSaveService.cs
@@ -1,5 +1,5 @@
-﻿using SocialEmpires.Models;
-using System.Text.Json;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialEmpires.Models;
 
 namespace SocialEmpires.Services
 {
@@ -12,25 +12,29 @@ namespace SocialEmpires.Services
             _appDbContext = appDbContext;
         }
 
-        private List<PlayerSave> LoadAllPlayerSaves()
-        {
-            return new List<PlayerSave>();
-        }
-
         public async Task<PlayerSave> CreatePlayerSaveAsync(string userId, string name)
         {
             var playerSave = PlayerSave.Create(userId, name);
+            await _appDbContext.PlayerSaves.AddAsync(playerSave);
             return playerSave;
         }
 
         public async Task<PlayerSave?> GetPlayerSaveAsync(string playerId)
         {
-            return _appDbContext.PlayerSaves.FirstOrDefault(_ => _.Pid == playerId);
+            return await _appDbContext.PlayerSaves
+                .Include(_ => _.PlayerInfo)
+                .Include(_ => _.Maps)
+                .Include(_ => _.PrivateState)
+                .FirstOrDefaultAsync(_ => _.Pid == playerId);
         }
 
         public async Task DeletePlayerSaveAsync(string playerId)
         {
-            throw new NotImplementedException();
+            var save = await _appDbContext.PlayerSaves.FirstOrDefaultAsync(_ => _.Pid == playerId);
+            if (save != null)
+            {
+                _appDbContext.Remove(save);
+            }
         }
     }
 }
