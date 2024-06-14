89818175d27f252ed851bf8bb8a59dac529f0bad
tonwin618 <tonwin618@qq.com>
2024-06-14 17:14:43 +0800

    feat: Add PlayerController

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialEmpires.Services;
+
+namespace SocialEmpires.Controllers
+{
+    [ApiController]
+    [Route("api/admin/players")]
+    public class PlayerController:ControllerBase
+    {
+        private readonly PlayerSaveService _playerSaveService;
+        public PlayerController(PlayerSaveService playerSaveService) 
+        {
+            _playerSaveService = playerSaveService;
+        }
+
+        [HttpPost("{playerId}/changeCash")]
+        public async Task ChangeCash(string playerId, [FromBody] int amount)
+        {
+            var save = await _playerSaveService.GetPlayerSaveAsync(playerId);
+            if (save == null)
+            {
+                return;
+            }
+            save.PlayerInfo.Cash += amount;
+        }
+    }
+}
