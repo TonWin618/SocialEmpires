ddd0eff99a6cc1e051c27ca878f286672aa33d2c
tonwin618 <tonwin618@qq.com>
2024-06-27 10:40:31 +0800

    feat: add NullRespectingSequenceEqual method to IEnumerable

---
+++ Utils/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialEmpires.Utils
+{
+    public static class EnumerableExtensions
+    {
+        public static bool NullRespectingSequenceEqual<T>(this IEnumerable<T>? first, IEnumerable<T>? second)
+        {
+            return first == null && second == null ? true
+                 : first == null || second == null ? false
+                 : first.SequenceEqual(second);
+        }
+    }
+}
