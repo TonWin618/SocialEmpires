14079780eb2739c100e86e1e69fa0ed486f6f551
tonwin618 <tonwin618@qq.com>
2024-06-27 17:53:28 +0800

    feat: AzureEmailSender no longer waits for the email to be sent completely

--- Infrastructure/EmailSender/AzureEmailSender.cs
+++ Infrastructure/EmailSender/AzureEmailSender.cs
@@ -22,25 +22,26 @@ namespace SocialEmpires.Infrastructure.EmailSender
 
         public void Send(string address, string subject, string content)
         {
-            _emailClient.Send(
-                Azure.WaitUntil.Completed,
+            var result = _emailClient.Send(
+                Azure.WaitUntil.Started,
                 _options.SenderAddress,
                 address,
                 subject,
                 content);
+            _logger.LogInformation("[subject:{subject}][address:{address}]", subject, address);
         }
 
         public async Task SendAsync(string address, string subject, string content, CancellationToken cancellationToken = default)
         {
             var result = await _emailClient.SendAsync(
-                Azure.WaitUntil.Completed,
+                Azure.WaitUntil.Started,
                 _options.SenderAddress,
                 address,
                 subject,
                 content,
                 null,
                 cancellationToken);
-            _logger.LogInformation("[subject:{subject}][address:{address}][status:{status}]", subject, address, result.Value.Status);
+            _logger.LogInformation("[subject:{subject}][address:{address}]", subject, address);
         }
     }
 }
