a578711f6c4704ed6ee1d0da0e4e9b678fe02cfd
tonwin618 <tonwin618@qq.com>
2024-06-28 17:41:27 +0800

    refactor: Config

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialEmpires.Services;
+
+namespace SocialEmpires.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public partial class AdminController : Controller
+    {
+        private readonly ConfigFileService _configFileService;
+
+        public AdminController(
+            ConfigFileService configFileService)
+        {
+            _configFileService = configFileService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PageResult<T> Page<T>(int pageIndex, int pageSize, int pageCount, IEnumerable<T>? data)
+        {
+            return new PageResult<T>(pageIndex, pageSize, pageCount, data?.Count() ?? 0, data);
+        }
+        public record PageResult<T>(int PageIndex, int PageSize, int PageCount, int DataCount, IEnumerable<T>? Data);
+    }
+}
