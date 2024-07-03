0433b4f71771bb9c45a5f4420fbd99e58abd3354
tonwin618 <tonwin618@qq.com>
2024-07-03 11:13:25 +0800

    feat: add Bulletin view.

---
+++ Controllers/AdminController.Bulletin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialEmpires.Controllers
+{
+    public partial class AdminController
+    {
+        [HttpGet]
+        public IActionResult Bulletin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult PublishBulletin([FromForm]string htmlContent)
+        {
+            return Redirect(Request.Headers.Referer);
+        }
+    }
+}
