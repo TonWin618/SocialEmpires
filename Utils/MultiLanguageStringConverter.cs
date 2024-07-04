6321124d3d300ff46f36756f7180e7d649c3cbc4
tonwin618 <tonwin618@qq.com>
2024-07-04 16:21:30 +0800

    fix: MultiLanguageStringConverter with parameterless constructor

--- Utils/MultiLanguageStringConverter.cs
+++ Utils/MultiLanguageStringConverter.cs
@@ -6,13 +6,6 @@ namespace SocialEmpires.Utils
 {
     public class MultiLanguageStringConverter : JsonConverter<MultiLanguageString>
     {
-        private readonly string _acceptLanguage;
-
-        public MultiLanguageStringConverter(IHttpContextAccessor accessor)
-        {
-            _acceptLanguage = accessor.HttpContext.Request.Headers.AcceptLanguage;
-        }
-
         public override MultiLanguageString? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             throw new NotImplementedException("Deserialization is not implemented.");
@@ -20,7 +13,7 @@ namespace SocialEmpires.Utils
 
         public override void Write(Utf8JsonWriter writer, MultiLanguageString value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.Get(_acceptLanguage));
+            writer.WriteStringValue(value.Zh);
         }
     }
 }
