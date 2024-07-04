679ab32bba2a78c39f95e7f18f71c78addebe2e9
tonwin618 <tonwin618@qq.com>
2024-07-04 15:03:07 +0800

    feat: add surpport for multi-languages.

---
+++ Models/MultiLanguageString.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialEmpires.Models
+{
+    [ComplexType]
+    public class MultiLanguageString
+    {
+        public string? En { get; set; }
+        public string? Zh { get; set; }
+
+        public string Get(string language)
+        {
+            return language switch
+            {
+                "zh" => Zh,
+                "en" => En,
+                _ => null
+            }?? "";
+        }
+
+        public void Set(string language, string content)
+        {
+            _ = language switch
+            {
+                "zh" => Zh = content,
+                "en" => En = content,
+                _ => null
+            };
+        }
+    }
+}
---
+++ Utils/MultiLanguageStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using SocialEmpires.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SocialEmpires.Utils
+{
+    public class MultiLanguageStringConverter : JsonConverter<MultiLanguageString>
+    {
+        private readonly string _acceptLanguage;
+
+        public MultiLanguageStringConverter(IHttpContextAccessor accessor)
+        {
+            _acceptLanguage = accessor.HttpContext.Request.Headers.AcceptLanguage;
+        }
+
+        public override MultiLanguageString? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException("Deserialization is not implemented.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, MultiLanguageString value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.Get(_acceptLanguage));
+        }
+    }
+}
