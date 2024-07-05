329a5a7afbf0c8862beb3c7af0ca2d3c6b871959
tonwin618 <tonwin618@qq.com>
2024-07-05 18:26:38 +0800

    feat: implement Read method of MultiLanguageString

--- Infrastructure/MultiLanguage/MultiLanguageStringConverter.cs
+++ Infrastructure/MultiLanguage/MultiLanguageStringConverter.cs
@@ -7,13 +7,61 @@ namespace SocialEmpires.Infrastructure.MultiLanguage
     {
         public override MultiLanguageString? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            throw new NotImplementedException("Deserialization is not implemented.");
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                if(reader.TokenType == JsonTokenType.String)
+                {
+                    return new MultiLanguageString(SupportLanguages.Default, reader.GetString()) { Current = SupportLanguages.Default };
+                }
+                else
+                {
+                    throw new JsonException();
+                }
+            }
+
+            string? en = null;
+            string? zh = null;
+            //add more languages...
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                {
+                    break;
+                }
+
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new JsonException();
+                }
+
+                string propertyName = reader.GetString() ?? throw new JsonException();
+                reader.Read();
+
+                switch (propertyName)
+                {
+                    case nameof(SupportLanguages.En):
+                        en = reader.GetString();
+                        break;
+                    case nameof(SupportLanguages.Zh):
+                        zh = reader.GetString();
+                        break;
+                    //add more languages...
+                    default:
+                        reader.Skip();
+                        break;
+                }
+            }
+
+            var result = new MultiLanguageString();
+            result.Set(SupportLanguages.En, en);
+            result.Set(SupportLanguages.Zh, zh);
+            //add more languages...
+            return result;
         }
 
         public override void Write(Utf8JsonWriter writer, MultiLanguageString value, JsonSerializerOptions options)
         {
-            int validContentCount = 0;
-
             if (value.Current != null)
             {
                 writer.WriteStringValue(value.GetCurrent());
@@ -22,8 +70,8 @@ namespace SocialEmpires.Infrastructure.MultiLanguage
             {
                 writer.WriteStartObject();
 
-                writer.WriteString(SupportLanguages.Zh, value.Zh);
-                writer.WriteString(SupportLanguages.En, value.En);
+                writer.WriteString(nameof(value.Zh), value.Zh);
+                writer.WriteString(nameof(value.En), value.En);
                 //Add more languages...
 
                 writer.WriteEndObject();
