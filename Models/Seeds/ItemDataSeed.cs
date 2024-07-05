22c9ad61755ad9a561ec95401697b6478f55de3b
tonwin618 <tonwin618@qq.com>
2024-07-05 17:54:02 +0800

    feat: add ItemDataSeed

---
+++ Models/Seeds/ItemDataSeed.cs
@@ -0,0 +1,22 @@
+﻿using SocialEmpires.Models.Configs;
+
+namespace SocialEmpires.Models.Seeds
+{
+    public static class ItemDataSeed
+    {
+        public static void Initialize(IServiceProvider serviceProvider, IEnumerable<Item> items)
+        {
+            using (var DbContext = serviceProvider.GetRequiredService<AppDbContext>())
+            {
+                if(DbContext.Items.Any())
+                {
+                    return;
+                }
+
+                DbContext.AddRange(items);
+
+                DbContext.SaveChanges();
+            }
+        }
+    }
+}
