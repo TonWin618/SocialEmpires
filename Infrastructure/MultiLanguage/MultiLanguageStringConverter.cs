ae42c9621acb8e0c514159e3936147a0a01c0134
tonwin618 <tonwin618@qq.com>
2024-07-05 15:40:37 +0800

    docs:

--- Infrastructure/MultiLanguage/MultiLanguageStringConverter.cs
+++ Infrastructure/MultiLanguage/MultiLanguageStringConverter.cs
@@ -24,6 +24,7 @@ namespace SocialEmpires.Infrastructure.MultiLanguage
 
                 writer.WriteString(SupportLanguages.Zh, value.Zh);
                 writer.WriteString(SupportLanguages.En, value.En);
+                //Add more languages...
 
                 writer.WriteEndObject();
             }
