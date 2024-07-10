58d47d43dc282782b26366ad8d290ecb2742debe
tonwin618 <tonwin618@qq.com>
2024-07-10 13:46:15 +0800

    feat: improve performance and use default language

--- Infrastructure/MultiLanguage/MultiLanguageJsonModelBinder.cs
+++ Infrastructure/MultiLanguage/MultiLanguageJsonModelBinder.cs
@@ -6,17 +6,35 @@ namespace SocialEmpires.Infrastructure.MultiLanguage
 {
     public class MultiLanguageJsonModelBinder : IModelBinder
     {
+        private static readonly Dictionary<string, JsonSerializerOptions> jsonSerializeroptions = CreateJsonSerializerOptionsList();
+
+        public static Dictionary<string, JsonSerializerOptions> CreateJsonSerializerOptionsList()
+        {
+            var jsonSerializeroptionsList = new Dictionary<string, JsonSerializerOptions>();
+            foreach (var language in SupportLanguages.List)
+            {
+                jsonSerializeroptionsList.Add(language, 
+                    new JsonSerializerOptions() 
+                    {
+                        
+                    }
+                    .WithLanguage(language));
+            }
+            return jsonSerializeroptionsList;
+        }
+
         public async Task BindModelAsync(ModelBindingContext bindingContext)
         {
             var request = bindingContext.HttpContext.Request;
             request.EnableBuffering();
+            request.Body.Position = 0;
 
             try
             {
                 var result = await JsonSerializer.DeserializeAsync(
                     request.Body, 
-                    bindingContext.ModelType, 
-                    new JsonSerializerOptions().WithLanguage(CultureInfo.CurrentCulture.Name));
+                    bindingContext.ModelType,
+                    jsonSerializeroptions[CultureInfo.CurrentCulture.Name ?? SupportLanguages.Default]);
 
                 bindingContext.Result = ModelBindingResult.Success(result);
             }
