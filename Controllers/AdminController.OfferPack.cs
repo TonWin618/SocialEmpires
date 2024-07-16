259fe9a44ad1973d04a222268e1f10f66d20323d
tonwin618 <tonwin618@qq.com>
2024-07-16 18:19:13 +0800

    feat: implement EnableOfferPack, DisableOfferPack, DeleteOfferPack in OfferPacks view.

--- Controllers/AdminController.OfferPack.cs
+++ Controllers/AdminController.OfferPack.cs
@@ -13,5 +13,29 @@ namespace SocialEmpires.Controllers
             (ViewData["PageCount"], ViewData["PageData"]) = PageHelper.Page(pageIndex, pageSize, _configFileService.OfferPacks);
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> EnableOfferPack(int id)
+        {
+            var offerPack = await _appDbContext.OfferPacks.FindAsync(id);
+            offerPack.Enabled = true;
+            return Redirect(Request.Headers.Referer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DisableOfferPack(int id)
+        {
+            var offerPack = await _appDbContext.OfferPacks.FindAsync(id);
+            offerPack.Enabled = false;
+            return Redirect(Request.Headers.Referer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteOfferPack(int id)
+        {
+            var offerPack = await _appDbContext.OfferPacks.FindAsync(id);
+            _appDbContext.Remove(offerPack);
+            return Redirect(Request.Headers.Referer);
+        }
     }
 }
