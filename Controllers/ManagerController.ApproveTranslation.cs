e4c27855a1e42b01cc7b8410992570e1111cb2bd
TonWin <tonwin618@qq.com>
2024-07-22 00:22:48 +0800

    feat: add ApproveTranslation view.

---
+++ Controllers/ManagerController.ApproveTranslation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialEmpires.Infrastructure.MultiLanguage;
+using SocialEmpires.Models.Notifications;
+using SocialEmpires.Utils;
+using System.Globalization;
+
+namespace SocialEmpires.Controllers
+{
+    public partial class ManagerController
+    {
+        public async Task<IActionResult> ApproveTranslation(int pageIndex = 1, int pageSize = 20)
+        {
+            ViewData["PageIndex"] = pageIndex;
+            ViewData["PageSize"] = pageSize;
+            ViewData["PageData"] = _appDbContext
+                .TranslationRecords
+                .Where(_ => _.Language == CultureInfo.CurrentCulture.Name)
+                .Page(pageIndex, pageSize, out var pageCount)
+                .ToList();
+            ViewData["PageCount"] = pageCount;
+            return View();
+        }
+    }
+}
