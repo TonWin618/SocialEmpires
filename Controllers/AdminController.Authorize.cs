ec9c8fd7baa97ec0302298669d020b1186c58575
tonwin618 <tonwin618@qq.com>
2024-07-22 17:55:29 +0800

    feat: add Authorize view.

---
+++ Controllers/AdminController.Authorize.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialEmpires.Utils;
+using System.Data;
+
+namespace SocialEmpires.Controllers
+{
+    public partial class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Authorize(
+            [FromServices] UserManager<IdentityUser> userManager, 
+            int pageIndex = 1, 
+            int pageSize = 20)
+        {
+            ViewData["PageIndex"] = pageIndex;
+            ViewData["PageSize"] = pageSize;
+            ViewData["PageData"] = userManager
+                .Users
+                .Page(pageIndex, pageSize, out var pageCount)
+                .Select(u => new UserDto(u.Id, u.Email, u.EmailConfirmed, userManager.GetRolesAsync(u).Result.ToList()))
+                .ToList();
+            ViewData["PageCount"] = pageCount;
+            return View();
+        }
+
+        public record UserDto(string Id, string Email, bool EmailConfirmed, List<string> Roles);
+    }
+}
--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SocialEmpires.Models;
 
 namespace SocialEmpires.Controllers
 {
     [Authorize(Roles = "Admin")]
     public partial class AdminController :Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public AdminController(
+            AppDbContext appDbContext) 
+        {
+            _appDbContext = appDbContext;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
